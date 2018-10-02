41e5f7225d002d112a8b2f32a8a671a2b41333c5
Behuamuh <sven1980@list.ru>
2018-10-02 04:17:33 +0300

    Выполнено дз3.

---
+++ validate.js
@@ -0,0 +1,31 @@
+$(function () {
+    $('form').submit(function() {
+        var namePtrn = '^[a-zа-яё ]{1,}$';
+        var phonePtrn = '^\\+7\\(\\d{3}\\)\\d{3}\\-?\\d{4}';
+        var emailPtrn = '^\\w+@\\w+\\.[a-z]+$';
+        var txtPtrn = '.';
+        var valid = checkElement($('#name'), namePtrn);
+        valid = checkElement($('#phone'), phonePtrn) && valid;
+        valid = checkElement($('#email'), emailPtrn) && valid;
+        valid = checkElement($('#textarea'), txtPtrn) && valid;
+        if (valid) {
+            return true;
+        }
+        return false;
+
+    });
+
+
+});
+function checkElement(elem,ptrn) {
+    var str = elem.val();
+    if (new RegExp(ptrn, 'igm').test(str)) {
+        elem.css('border','');
+        return true;
+    }else {
+        elem.css('border','1px solid red');
+        //console.log('error');
+        return false;
+    }
+
+}
